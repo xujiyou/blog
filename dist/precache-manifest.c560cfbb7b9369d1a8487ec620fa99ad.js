c3b4a9b88cfa6b706c2a063e27c103fd1f725544
xujiyou <xujiyou555@gmail.com>
2019-10-30 13:56:09 +0800

    init

---
+++ dist/precache-manifest.c560cfbb7b9369d1a8487ec620fa99ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a253846339285d0d2a9",
+    "url": "/css/app.a884c785.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "883ffa95e4cf40ed1eb9912897c85033",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aefe9788972df23dc95c",
+    "url": "/js/about.23439fe2.js"
+  },
+  {
+    "revision": "3a253846339285d0d2a9",
+    "url": "/js/app.f8500890.js"
+  },
+  {
+    "revision": "1cc6c5f71c75b6984532",
+    "url": "/js/chunk-vendors.996f3fb1.js"
+  },
+  {
+    "revision": "86af71660e5fa371f45c8e48da13101f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
