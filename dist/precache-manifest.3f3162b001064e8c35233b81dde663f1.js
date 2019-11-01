75d16c6da904dae262fb22e3198b229af7d2f9ec
xujiyou <xujiyou555@gmail.com>
2019-11-01 11:35:59 +0800

    点击按钮跳转

---
+++ dist/precache-manifest.3f3162b001064e8c35233b81dde663f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f8a07c768948ba16e3",
+    "url": "dist/css/app.css"
+  },
+  {
+    "revision": "710b2a22c5cf5a299bb510ffc6c8718b",
+    "url": "dist/index.html"
+  },
+  {
+    "revision": "20f8a07c768948ba16e3",
+    "url": "dist/js/app.js"
+  },
+  {
+    "revision": "442314433b403e104ed8",
+    "url": "dist/js/chunk-2d22d746.js"
+  },
+  {
+    "revision": "b75a460d278b700480ec",
+    "url": "dist/js/chunk-vendors.js"
+  },
+  {
+    "revision": "86af71660e5fa371f45c8e48da13101f",
+    "url": "dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "dist/robots.txt"
+  },
+  {
+    "revision": "caa652561f49a34cd3deb5cb480c3730",
+    "url": "dist/xu.jpeg"
+  }
+]);
